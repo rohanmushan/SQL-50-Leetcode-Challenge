74b779a6a2d34006fc3bcc037f6ce0c6e617ff72
Rohan Mushan <97397494+rohanmushan@users.noreply.github.com>
2025-08-30 23:46:56 +0530

    Update 17-Project Employees I.sql

--- 17-Project Employees I.sql
+++ 17-Project Employees I.sql
@@ -61,3 +61,8 @@
 -- +-------------+---------------+
 -- Explanation: The average experience years for the first project is (3 + 2 + 1) / 3 = 2.00 and for the second project is (3 + 2) / 2 = 2.50
  
+SELECT p.project_id, ROUND(avg(e.experience_years),2) 
+AS average_years FROM Project p
+LEFT JOIN Employee e
+ON p.employee_id=e.employee_id
+GROUP BY project_id;
